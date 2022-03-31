88b0ae007381c4017fbed6084392de0cdb32c181
Junaid Ghafoor <35875921+JunaidG4@users.noreply.github.com>
2022-03-31 19:21:11 +0100

    finalised code for fetching data and sending

--- pages/staffpanel/enrolledCadets.js
+++ pages/staffpanel/enrolledCadets.js
@@ -1,79 +1,50 @@
-import Head from "next/head";
-import ReadFirestore from "../../firebase/readFirestore";
+import Head from "next/dist/shared/lib/head";
 import { collection, setDoc, addDoc, onSnapshot, doc } from "firebase/firestore";
-import { db, colRef } from "../../firebase/firebaseInit";
 import { useEffect, useState } from "react";
+import { db, colRef } from "../../firebase/firebaseInit";
 
-export const getStaticProps = async () => {
 
-    const res = await fetch('https://jsonplaceholder.typicode.com/users');
-    const data = await res.json();
 
-    return {
-        props: { cadets: data }
-    }
+  const handleCadet = async () => {
+    const forename = prompt("Enter forename:")
+    const surname = prompt("Enter surname:")
+    const payload = {forename, surname};
+    const docRef = await addDoc(colRef, payload);
+    console.log("New ID is: " + docRef.id);
 }
+  
+  export default function enrolledCadets() {
 
-export const getTheCadets = async () => {
-
-    const[cadetsNames, setCadets] = useState([]);
-
-        useEffect(
-            () => 
-            onSnapshot(collection(db, "cadets"), (snapshot) =>
-                setCadets(snapshot.docs.map(doc => doc.data()))
-            )
+    const [cadets, setCadets] = useState([{ forename: "Loading...", surname: "Loading..." }]);
+  
+    useEffect(
+      () =>
+        onSnapshot(collection(db, "cadets"), (snapshot) =>
+          setCadets(snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id })))
         ),
-    []
-
-    return {
-        cadetsNames
-    }
-
-}
-
-const enrolledCadets = ({cadets}) => {
-
-    const handleCadet = async () => {
-        const forename = prompt("Enter forename:")
-        const surname = prompt("Enter surname:")
-        const payload = {forename, surname};
-        const docRef = await addDoc(colRef, payload);
-        console.log("New ID is: " + docRef.id);
-    }
-
-
-    return ( 
+      []
+    );
+  
+    return (
         <>
-        <Head>
-            <title>CMS | Enrolled Cadets</title>
-        </Head>
-        <div>
-        <ReadFirestore />
-        <button className="button" onClick={handleCadet}>Enroll Cadet</button>
-        <form className="add" > 
-        <label for="forename">Forename:</label>
-        <input type="text" name="forename" required />
-        <label label for="surname">Surname:</label>
-        <input type="text" name="surname" required />
-        <button className="button" onClick={handleCadet}>Enroll Cadet</button>
-        </form>
-
-            <h1>Enrolled Cadets</h1>
-            {cadets.map(cadet => (
-                <div className="cadetnames">
-                    <div key={cadet.id}> 
-                        <a>
-                            <h3>CDT { cadet.name }</h3>
-                            <h4>Username: { cadet.username }</h4>
-                        </a>
-                    </div>
-                </div>
-            ))}
-        </div>
+          <Head>
+              <title>CMS | Enrolled Cadets</title>
+          </Head>
+          <div>
+          {cadets.map((cadet) => (
+              <div className="cadetnames">
+                  <div key={(cadet.id)}>
+                      <a>
+                          <h3>{cadet.forename} {cadet.surname}</h3>
+                      </a>
+                  </div>
+              </div>
+          ))}
+          <div className="addCadet">
+          <button className="button" onClick={handleCadet}>Add Cadet</button>
+          </div>
+          </div>
         </>
-     );
-}
- 
-export default enrolledCadets;
-
+      
+    );
+  }
