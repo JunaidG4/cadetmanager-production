a0b44c1e562ade1cdb3e0841709d54ecd0f2b545
Junaid Ghafoor <35875921+JunaidG4@users.noreply.github.com>
2022-03-31 15:14:07 +0100

    removed broken auth

--- pages/_app.js
+++ pages/_app.js
@@ -2,16 +2,16 @@
 
 import Layout from '../Components/Layout'
 import '../styles/globals.css'
-import authContextProvider from '../context/authContext'
+
 
 
 function MyApp({ Component, pageProps }) {
   return (
-    <authContextProvider>
+
     <Layout>
       <Component {...pageProps} />
     </Layout>
-    </authContextProvider>
+
 
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -6,13 +6,13 @@ import Navbar from '../Components/Navbar';
 import styles from '../styles/Home.module.css';
 import firebase from '../firebase/firebaseInit';
 import readFirestore from '../firebase/readFirestore'
-import { useAuth } from '../context/authContext';
+
 
 firebase();
 
 
 export default function Home() {
-  const {currentUser} = useAuth()
+
   return (
     <>
       <Head>
@@ -20,7 +20,7 @@ export default function Home() {
       </Head>
       <div>
         <h1>Welcome to CMS</h1>
-        <text>The current user is: ${currentUser}</text>
+
         <p>
           Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
           been the industry's standard dummy
