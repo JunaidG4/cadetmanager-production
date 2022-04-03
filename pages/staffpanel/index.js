7052a0f8c71869ae262300ce2e12a7f00e9c7da1
hamzah.ghafoor <hamzah.ghafoor@radically.digital>
2022-04-03 16:59:55 +0100

    added some crap

--- pages/staffpanel/index.js
+++ pages/staffpanel/index.js
@@ -1,84 +1,88 @@
-import { 
-    signInWithEmailAndPassword,
-    onAuthStateChanged,
-    signOut 
-} from "firebase/auth";
-import Head from "next/head";
-import { useRouter } from "next/router";
-import { useState } from "react";
-import { auth } from "../../firebase/firebaseInit";
-
-
+import { signInWithEmailAndPassword, onAuthStateChanged, signOut } from 'firebase/auth';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+import { useState } from 'react';
+import { auth } from '../../firebase/firebaseInit';
 
 export default function StaffLogin() {
-    const router = useRouter();
+  const router = useRouter();
 
-    const handleClick = e => {
-        e.preventDefault()
-        router.push('/staffpanel/staffhub')
+  const handleClick = (e) => {
+    e.preventDefault();
+    router.push('/staffpanel/staffhub');
+  };
+
+  const [user, setUser] = useState({});
+
+  onAuthStateChanged(auth, (currentUser) => {
+    setUser(currentUser);
+  });
+
+  const [signInEmail, setSignInEmail] = useState('');
+  const [signInPassword, setSignInPassword] = useState('');
+
+  const login = async (e) => {
+    e.preventDefault();
+    console.log(e.target);
+    try {
+      const user = await signInWithEmailAndPassword(auth, signInEmail, signInPassword);
+      router.push('/staffpanel/staffhub');
+      console.log(user);
+    } catch (error) {
+      console.log(error.message);
     }
+  };
 
-    const [user, setUser] = useState({});
-
-    onAuthStateChanged(auth, (currentUser) => {
-        setUser(currentUser);
-    });
-
-    const [signInEmail, setSignInEmail] = useState("")
-    const [signInPassword, setSignInPassword] = useState("")
-
-
-    const login = async (e) => {
-        e.preventDefault();
-        console.log(e.target);
-        try {
-            const user = await signInWithEmailAndPassword (
-                auth,
-                signInEmail,
-                signInPassword
-            );
-            router.push('/staffpanel/staffhub')
-            console.log(user);
-        } catch (error) {
-            console.log(error.message);
-        }
-    }
-
-    
-
-    const logout = async () => {
+  const logout = async () => {
     await signOut(auth);
-    };
+  };
 
-    
-    return ( 
-        <>
-        
-            <Head>
-            <title> CMS | Staff Login</title>
-            </Head>
-            <div>
-                <div className="login-form p-12">
-                    <form className="login">
-                        <h1>Staff Login Area</h1>
-                        <div className="form-content">
-                        <div className="input-field">
-                            <input type="email" required placeholder="Email" autoComplete="nope"  onChange={(event) => {setSignInEmail(event.target.value)}}/>
-                        </div>
-                        <div className="input-field">
-                            <input type="password" required placeholder="Password" autoComplete="new-password"  onChange={(event) => {setSignInPassword(event.target.value)}}/>
-                        </div>
-                        </div>
-                        <div className="action">
-                        <button type="submit"  onClick={(e) => {
-
-                        login(e)}}>Sign in</button>
-                        </div>
-                    </form>
-                    </div>
+  return (
+    <>
+      <Head>
+        <title> CMS | Staff Login</title>
+      </Head>
+      <div>
+        <div className="login-form p-12">
+          <form className="login">
+            <h1 className=" text-lg">Staff Login Area</h1>
+            <div className="form-content">
+              <div className="input-field">
+                <input
+                  type="email"
+                  required
+                  placeholder="Email"
+                  autoComplete="nope"
+                  onChange={(event) => {
+                    setSignInEmail(event.target.value);
+                  }}
+                />
+              </div>
+              <div className="input-field">
+                <input
+                  type="password"
+                  required
+                  placeholder="Password"
+                  autoComplete="new-password"
+                  onChange={(event) => {
+                    setSignInPassword(event.target.value);
+                  }}
+                />
+              </div>
             </div>
-        </>
-    );
+            <div className="action">
+              <button
+                type="submit"
+                onClick={(e) => {
+                  login(e);
+                }}
+              >
+                Sign in
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
 }
-
- 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,26 @@
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}"
-  ],
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    container: {
+      padding: {
+        DEFAULT: '1rem',
+      },
+    },
+    fontSize: {
+      'xs': '.75rem',
+      'sm': '.875rem',
+      'tiny': '.875rem',
+      'base': '1rem',
+      'lg': '1.25rem',
+      'xl': '1.5rem',
+      '2xl': '2.5rem',
+      '3xl': '3.5rem',
+      '4xl': '5.438rem',
+      '5xl': '6rem',
+      '6xl': '7rem',
+      '7xl': '8rem',
+    },
     extend: {},
   },
   plugins: [],
-}
+};
